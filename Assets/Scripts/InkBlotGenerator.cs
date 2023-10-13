2386a901ca1e336b74d4ba1511452ca9ae2e4985
g6bby <nabag123@newschool.edu>
2023-10-13 16:58:54 -0400

    created scale increase of ink

--- Assets/Scripts/InkBlotGenerator.cs
+++ Assets/Scripts/InkBlotGenerator.cs
@@ -4,31 +4,42 @@ using UnityEngine;
 
 public class InkBlotGenerator : MonoBehaviour
 {
-    public GameObject inkblotPrefab; // Assign your inkblot prefab in the Inspector
-    public Transform[] pathWaypoints; // Define the path waypoints in the Inspector
+    public GameObject inkblotPrefab;
+    public Transform[] pathWaypoints;
+
     public float minInterval = 1.0f; 
     public float maxInterval = 5.0f;
 
+    private float initialScale = 2.0f;
+    private float scaleIncreaseAmount = 2.0f;
+
     private int currentWaypointIndex = 0;
     private float nextSpawnTime = 0f;
+    private GameObject lastInkblot;
 
     void Start()
     {
-        // Set the initial spawn time
         nextSpawnTime = Time.time + Random.Range(minInterval, maxInterval);
+
+        lastInkblot = Instantiate(inkblotPrefab, pathWaypoints[0].position, Quaternion.Euler(90f, 0f, 0f));
+        lastInkblot.transform.localScale = new Vector3(initialScale, initialScale, initialScale);
     }
 
     void Update()
     {
         if (Time.time >= nextSpawnTime && currentWaypointIndex < pathWaypoints.Length)
         {
-            // Instantiate an inkblot prefab at the current waypoint position
-            Instantiate(inkblotPrefab, pathWaypoints[currentWaypointIndex].position, Quaternion.Euler(90f, 0f, 0f));
+            if (lastInkblot != null)
+            {
+                float newScale = lastInkblot.transform.localScale.x + scaleIncreaseAmount;
+                lastInkblot.transform.localScale = new Vector3(newScale, newScale, newScale);
+            }
 
-            // Move to the next waypoint
+            GameObject newInkblot = Instantiate(inkblotPrefab, pathWaypoints[currentWaypointIndex].position, Quaternion.Euler(90f, 0f, 0f));
+            lastInkblot = newInkblot;
+
             currentWaypointIndex++;
 
-            // Set the time for the next spawn
             nextSpawnTime = Time.time + Random.Range(minInterval, maxInterval);
         }
     }
