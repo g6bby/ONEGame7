fdb83fb05bd77598f1f2bbe2e0677228ea20858e
g6bby <nabag123@newschool.edu>
2023-10-13 16:18:59 -0400

    updated inkblot

--- Assets/Scripts/InkBlotGenerator.cs
+++ Assets/Scripts/InkBlotGenerator.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class InkBlotGenerator : MonoBehaviour
 {
-    public GameObject inkblotPrefab; 
+    public GameObject inkblotPrefab; // Assign your inkblot prefab in the Inspector
+    public Transform[] pathWaypoints; // Define the path waypoints in the Inspector
     public float minInterval = 1.0f; 
     public float maxInterval = 5.0f;
 
-    private float nextInkblotTime;
+    private int currentWaypointIndex = 0;
+    private float nextSpawnTime = 0f;
 
     void Start()
     {
-        nextInkblotTime = Time.time + Random.Range(minInterval, maxInterval);
+        // Set the initial spawn time
+        nextSpawnTime = Time.time + Random.Range(minInterval, maxInterval);
     }
 
     void Update()
     {
-        if (Time.time >= nextInkblotTime)
+        if (Time.time >= nextSpawnTime && currentWaypointIndex < pathWaypoints.Length)
         {
-            Vector3 randomPosition = new Vector3(
-                Random.Range(-10f, 10f), // Adjust the range as needed
-                2.51f, 
-                Random.Range(-10f, 10f)
-            );
+            // Instantiate an inkblot prefab at the current waypoint position
+            Instantiate(inkblotPrefab, pathWaypoints[currentWaypointIndex].position, Quaternion.Euler(90f, 0f, 0f));
 
-            GameObject newInkblot = Instantiate(inkblotPrefab, randomPosition, Quaternion.identity);
+            // Move to the next waypoint
+            currentWaypointIndex++;
 
-            newInkblot.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
-
-            nextInkblotTime = Time.time + Random.Range(minInterval, maxInterval);
+            // Set the time for the next spawn
+            nextSpawnTime = Time.time + Random.Range(minInterval, maxInterval);
         }
     }
 }
